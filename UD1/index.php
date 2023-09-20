0332a74b0101f58b6ee300db027f84ad599b8a96
Pablo Mérida Velasco <a21mevepa@iesgrancapitan.org>
2023-09-20 10:14:13 +0200

    hello_world_UD1

---
+++ UD1/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hola que tal</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1 class="name">Pablo Mérida</h1>
+    <?php
+        include("main.php");
+    ?>
+    <a href="https://github.com/pmerida08/HolaMundoPHP.git">Ver en github</a>
+</body>
+</html>
